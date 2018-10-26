a58e18389964f955f722de586e2a1de6187f3e69
PANAGIWTIS PETROPOULAKIS <sdi1500129@linux22.di.uoa.gr>
2018-10-26 14:14:39 +0300

    Exhaustive-Search-Done-Minor-Changes-Remaining

---
+++ neighborsProblem/model/exhaustiveSearch/exhaustiveSearch.cc
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <cmath>
+#include "exhaustiveSearch.h"
+#include "../../hashFunction/hashFunction.h"
+#include "../../item/item.h"
+#include "../../utils/utils.h"
+
+using namespace std;
+
+/////////////////////////////////////////
+/* Implementation of exhaustive search */
+/////////////////////////////////////////
+
+/* Default constructor */
+exhaustiveSearch::exhaustiveSearch():tableSize(0),n(0),dim(0),fitted(0){}
+
+exhaustiveSearch::~exhaustiveSearch(){}
+
+/* Save given points */
+void exhaustiveSearch::fit(list<Item>& points, errorCode& status){
+    
+    /* Iteratiors */
+    list<Item>::iterator iterPoints = points.begin(); // Iterate through points
+   
+    status = SUCCESS;
+
+    /* Already fitted */
+    if(this->fitted == 1){
+        status = METHOD_ALREADY_USED;
+        return;
+    }
+
+    /* Check parameters */
+    if(points.size() > MAX_DIM || points.size() <= 0){
+        status = INVALID_DIM;
+        return;
+    }
+
+    /* Set members */
+    this->n = points.size();
+    if(this->n < MIN_POINTS || this->n > MAX_POINTS){
+        status = INVALID_POINTS;
+        return;
+    }
+ 
+    /* Copy points */
+    for(iterPoints = points.begin(); iterPoints != points.end(); iterPoints++)
+        this->points.push_back(*iterPoints);
+
+    this->fitted = 1;
+}
+
+/* Find the radius neighbors of a given point */
+void exhaustiveSearch::radiusNeighbors(Item& query, int radius, list<Item>& neighbors, list<double>* neighborsDistances, errorCode& status){
+    int queryDim = query.getDim();
+    int i;
+    double currDist; // Distance of a point in list
+
+    status = SUCCESS;
+
+    /* Check parameters */
+    if(queryDim <= 0 || queryDim > MAX_DIM || queryDim != this->dim){
+        status = INVALID_DIM;
+        return;
+    }
+
+    if(radius < MIN_RADIUS || radius > MAX_RADIUS){
+        status = INVALID_RADIUS;
+        return; 
+    }
+
+    /* Check model */
+    if(this->fitted == 0){
+        status = METHOD_UNFITTED;
+        return;
+    }
+
+    /* Clear given lists */
+    neighbors.clear();
+    if(neighborsDistances != NULL)
+        neighborsDistances->clear();
+
+    /* Scann all points */
+    for(i = 0; i < this->tableSize; i++){
+        /* Find current distance */
+        currDist = this->points[i].euclideanDist(query, status);
+        if(status != SUCCESS)
+            return;
+
+        /* Keep neighbor */
+        if(currDist < radius){
+            neighbors.push_back(this->points[i]);
+            if(neighborsDistances != NULL)
+                neighborsDistances->push_back(currDist);
+        }
+    } // End for
+}
+
+/* Find the nearest neighbor of a given point */
+void exhaustiveSearch::nNeighbor(Item& query, Item& nNeighbor, double* neighborDistance, errorCode& status){
+    int queryDim = query.getDim();
+    int i, posMin;
+    double minDist = -1; // Current minimum distance 
+    double currDist; // Distance of a point in list
+
+    status = SUCCESS;
+
+    /* Check parameters */
+    if(queryDim <= 0 || queryDim > MAX_DIM || queryDim != this->dim){
+        status = INVALID_DIM;
+        return;
+    }
+
+    /* Check model */
+    if(this->fitted == 0){
+        status = METHOD_UNFITTED;
+        return;
+    }
+
+    /* Scann all points */
+    for(i = 0; i < this->tableSize; i++){
+        
+        /* Get current distance from query */
+        currDist = this->points[i].euclideanDist(query, status);
+        if(status != SUCCESS)
+            return;
+    
+        if(i == 0){
+            posMin = i;
+            minDist = currDist;
+        }
+        else if(minDist > currDist){
+            posMin = i;
+            minDist = currDist;
+            
+        }
+    } // End for
+
+    /* Set nearest neighbor */
+    nNeighbor = this->points[posMin];
+    if(neighborDistance != NULL)
+        *neighborDistance = minDist;
+}
+
+
+///////////////
+/* Accessors */
+///////////////
+
+int exhaustiveSearch::getNumberOfPoints(errorCode& status){
+    status = SUCCESS;
+
+    if(fitted == 0){
+        status = METHOD_UNFITTED;
+        return -1;
+    }
+    else
+        return this->n;
+}
+
+int exhaustiveSearch::getDim(errorCode& status){
+    status = SUCCESS;
+
+    if(fitted == 0){
+        status = METHOD_UNFITTED;
+        return -1;
+    }
+    else
+        return this->dim;
+}
+
+
+/* Print statistics */
+void exhaustiveSearch::print(void){
+
+    if(this->fitted == 0)
+        cout << "Method unfitted\n";
+    else{
+
+        cout << "Exhaustive search statistics\n";
+        cout << "Dimension: " << this->dim << "\n";
+        cout << "Total points: " << this->n << "\n\n";
+    }
+}
+
+void exhaustiveSearch::printHashFunctions(void){
+    cout << "Exhaustive search hans't hash fucntions\n\n";
+}
+
+// Petropoulakis Panagiotis
---
+++ neighborsProblem/model/exhaustiveSearch/exhaustiveSearch.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <list>
+#include "../model.h"
+#include "../../item/item.h"
+#include "../../utils/utils.h"
+
+/* Neighbors problem using exhaustice search */
+class exhaustiveSearch: public model{
+    private:
+        std::vector<Item> points; // All points are be kept in a single table
+        int tableSize; // == n
+        int n; // Number of items 
+        int dim; // Dimension
+        int fitted; // Method is fitted with data
+    public:
+
+        exhaustiveSearch();
+        ~exhaustiveSearch();
+
+        void fit(std::list<Item>& points, errorCode& status);
+
+        void radiusNeighbors(Item& query, int radius, std::list<Item>& neighbors, std::list<double>* neighborsDistances, errorCode& status);
+        void nNeighbor(Item& query, Item& nNeighbor, double* neighborDistance, errorCode& status);
+        
+        int getNumberOfPoints(errorCode& status);
+        int getDim(errorCode& status);
+
+        void print(void);
+        void printHashFunctions(void);
+};
+
+// Petropoulakis Panagiotis
